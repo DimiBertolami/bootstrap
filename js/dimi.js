adb3625131b72ea99c8cbead6e1a806f09e8fb47
Dimitri Bertolami <dimibertolami@gmail.com>
2022-04-22 14:12:31 +0200

    still working on the vanishing line issue.....

--- js/dimi.js
+++ js/dimi.js
@@ -1,6 +1,6 @@
 function removeLine($elementId) {
-    console.log($elementId);
     let $Item = document.getElementById("card8Line");
+    console.log($elementId);
     $Item.hidden = true;
     console.log("removing line from card 8")
 }
